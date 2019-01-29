89cf16d5781f4fcbcfd15a6ebb113cb174b41acf
Mateusz Gorczycki <47029250+Trixie94@users.noreply.github.com>
2019-01-29 13:08:24 +0100

    Secret level and teleports
    Added secret level.
    FIxed teleport scripts.

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -5,20 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class Teleport : MonoBehaviour
 {
-    Scene scene;
+    public int index;
     public void OnTriggerEnter2D(Collider2D coll)
     {
         if (coll.gameObject.tag == "Player") 
         {
-            scene = SceneManager.GetActiveScene();
-            if (scene.name == "Level1")
-            {
-                SceneManager.LoadScene("Level2");
-            }
-            if (scene.name == "Level2")
-            {
-                SceneManager.LoadScene("Level3");
-            }
+            SceneManager.LoadScene(index);
         }
     }
 }
