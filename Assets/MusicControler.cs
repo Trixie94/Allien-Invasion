4806bf0e532382cf96849075b33c8de6386efd2e
Mateusz Gorczycki <47029250+Trixie94@users.noreply.github.com>
2019-02-02 01:05:05 +0100

    Sliders and music menu
    Fixed sliders.
    Fixed music menu.

---
+++ Assets/MusicControler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MusicControler : MonoBehaviour
+{
+    public AudioMixer mixer;
+   
+
+    public void SetLevelMusic(float sliderValue)
+    {
+        mixer.SetFloat("Music", Mathf.Log10(sliderValue)*20);
+    }
+
+    public void SetLevelSFX(float sliderValue)
+    {
+        mixer.SetFloat("Sound Effects", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void SetLevelMaster(float sliderValue)
+    {
+        mixer.SetFloat("Master", Mathf.Log10(sliderValue) * 20);
+    }
+}
